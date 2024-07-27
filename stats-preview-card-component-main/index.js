9944926ec1db4a5b3a488b7f9ddfad33183e370b
SaintJachriz <mbagwujachi03@gmail.com>
2024-07-27 23:05:26 +0100

    Add files via upload

---
+++ stats-preview-card-component-main/index.js
@@ -0,0 +1,11 @@
+function Show() {
+    const hide = document.getElementById('hidden');
+
+    hide.style.display = 'flex';
+}
+
+function Hide() {
+    const hide = document.getElementById('hidden');
+
+    hide.style.display = 'none';
+}
